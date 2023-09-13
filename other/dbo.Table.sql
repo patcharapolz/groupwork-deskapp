298a22f2fa696976a26b13eebc684177ec9842c0
usar <cffemate@gmail.com>
2023-09-13 13:48:13 +0700

    remove file

--- other/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Province] NCHAR(10) NULL, 
-    [Road] NCHAR(10) NULL, 
-    [Distract] NCHAR(10) NULL, 
-    [Sub_Distract] NCHAR(10) NULL
-)
